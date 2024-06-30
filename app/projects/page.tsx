cc6581875f383a0b563140d1fda4a1019dfeff42
John Paul <iamjohnpaulr5@gmail.com>
2024-07-01 02:30:52 +0530

    refactor: Improve ProjectsPage content and add link to Github

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -24,13 +24,16 @@ const ProjectsPage = () => {
           <Typography className="my-6 leading-relaxed">
             I am{" "}
             <span className="text-primary/80">
-              passionate about creating new projects
+              passionate about creating meaningful projects
             </span>
             , as it is the greatest key to gaining knowledge. On this page, you can navigate to{" "}
             <span className="text-primary/80">
-              the&nbsp;{getTotalProjects()}+&nbsp;applications
+              the&nbsp;{getTotalProjects()}+&nbsp;main applications
             </span>{" "}
             that I have developed.
+            <span className="block mt-4">
+              View more projects on my <a target="_blank" href="https://github.com/RJohnPaul" className=" hover:underline-offset-2 hover:text-purple-500 transition-all underline cursor-pointer">Github!</a>
+            </span>
 
 
           </Typography>
